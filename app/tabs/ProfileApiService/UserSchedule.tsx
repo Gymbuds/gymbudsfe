43fd06fe153085ac25a50eaa6f6d9907f97a8e5c
Victor Yuen <92613498+victoryuen@users.noreply.github.com>
2025-03-16 18:28:19 -0400

    on focus change profile text a bit different on profile + fully working front to back end user schedule

--- app/tabs/ProfileApiService/UserSchedule.tsx
+++ app/tabs/ProfileApiService/UserSchedule.tsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, ScrollView, TouchableOpacity, Modal, TextInput } from 'react-native';
+import { View, Text, ScrollView, TouchableOpacity, Modal, TextInput,Alert} from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { fetchFunctionWithAuth } from '@/api/auth';
-import { formatTime } from '@/app/utils/util';
+import { formatTime, validateTime,convertTo24Hour,padTime} from '@/app/utils/util';
 import tw from 'twrnc';
 
 // Define navigation types
@@ -17,7 +16,7 @@ type RootStackParamList = {
   Profile: undefined;
   Schedule: undefined;
 };
-interface TimeRange {
+export interface TimeRange {
   id: number;
   user_id: number;
   day_of_week: string;
@@ -45,16 +44,20 @@ export default function ScheduleScreen({ navigation }: Props) {
 
   const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-  const postUserTimeRange = async () => {
-    // console.log(startTimeAdd, startTimePeriod, endTimeAdd, endTimePeriod);
-    // try {
-    //   const data = await fetchFunctionWithAuth('avalrange', {
-    //     method: 'GET',
-    //   });
-    //   setTimeRanges(data);
-    // } catch (error) {
-    //   console.error('Failed to fetch user time ranges', error);
-    // }
+  const postUserTimeRange = async (day:string,start_hour:number,start_minutes:number,end_hour:number,end_minutes:number) => {
+    const start_time = padTime(start_hour, start_minutes);
+    const end_time = padTime(end_hour, end_minutes);
+    
+    try {
+      const data = await fetchFunctionWithAuth('avalrange/create', {
+        method: 'POST',
+        body: JSON.stringify({ day_of_week:day,start_time,end_time}),
+      });
+      setModalVisible(false)
+      getUserTimeRanges()
+    } catch (error) {
+      console.error('Failed to create user time range', error);
+    }
   };
 
   const getUserTimeRanges = async () => {
@@ -67,21 +70,55 @@ export default function ScheduleScreen({ navigation }: Props) {
       console.error('Failed to fetch user time ranges', error);
     }
   };
-
+  const deleteUserTimeRange = async (aval_range_id) => {
+    try {
+      const data = await fetchFunctionWithAuth('avalrange',{
+        method: 'DELETE',
+        body: JSON.stringify({id:aval_range_id})
+      });
+    } catch (error) {
+      console.error('Failed to  delete time ranges', error);
+    }
+  };
   useEffect(() => {
     getUserTimeRanges();
   }, []);
 
-  useEffect(() => {
-    console.log(timeRanges);
-  }, [timeRanges]);
-
+  // useEffect(() => {
+  //   console.log(timeRanges);
+  // }, [timeRanges]);
+  const wrongTimeAlert = () =>{
+    Alert.alert('Invalid Time', 'Please choose a proper Time', [
+      {
+        text: 'Ok',
+        onPress: () => console.log('Okay Pressed'),
+        style: 'default',
+      }
+      
+    ])
+  }
+  const handleDeleteTimeRange = async (range_id : number) =>{
+    await deleteUserTimeRange(range_id);
+    getUserTimeRanges(); 
+  }
   const handleAddTimeRange = ({ day, start, end }) => {
-    console.log('hi')
-    console.log(day,start,end)
-    postUserTimeRange();
-    // Add logic to handle adding time range
-  };
+    if (!(validateTime(start) && validateTime(end))) {
+      return wrongTimeAlert();
+    }
+    let startTime = convertTo24Hour(start);
+  let endTime = convertTo24Hour(end);
+
+  if (
+    (startTime.period === endTime.period && 
+     (startTime.hour < endTime.hour || (startTime.hour === endTime.hour && startTime.minute < endTime.minute))) ||
+    (startTime.period === "AM" && endTime.period === "PM") ||
+    (startTime.period === "PM" && endTime.period === "AM")
+  ) {
+    return postUserTimeRange(day, startTime.hour, startTime.minute, endTime.hour, endTime.minute);
+  }
+
+  return wrongTimeAlert(); // if no conditon met show the error 
+};
 
   const handleStartOpen = (open) => {
     if (open) {
@@ -130,7 +167,7 @@ export default function ScheduleScreen({ navigation }: Props) {
                 <Text style={tw`text-sm text-gray-700`}>
                   {formatTime(range.start_time)} - {formatTime(range.end_time)}
                 </Text>
-                <TouchableOpacity onPress={() => setTimeRanges(timeRanges.filter((r) => r.id !== range.id))}>
+                <TouchableOpacity onPress={() => handleDeleteTimeRange(range.id)}>
                   <Icon name="times" size={16} color="gray" />
                 </TouchableOpacity>
               </View>
@@ -148,49 +185,51 @@ export default function ScheduleScreen({ navigation }: Props) {
             <View style={tw`bg-white p-6 rounded-lg w-80`}>
               <Text style={tw`text-lg font-bold mb-4`}>Add Time Range</Text>
 
-              <Text style={tw`text-xs text-gray-500 mb-2`}>Start Time</Text>
-              <View style={tw`flex-row items-center mb-4`}>
-                <TextInput
-                  style={tw`border p-2 rounded flex-1 mr-2`}  // Added flex-1 and margin-right
-                  placeholder="7:00"
-                  value={startTimeAdd}
-                  onChangeText={setStartTimeAdd}
-                />
-                <DropDownPicker
-                  placeholder='AM/PM'
-                  open={startOpen}
-                  setOpen={handleStartOpen}
-                  containerStyle={tw`h-10 flex-1`}
-                  style={tw`border p-2 rounded bg-white text-black`}
-                  items={items}
-                  setItems={setItems}
-                  value={startValue}
-                  setValue={setStartValue}
-                  onChangeValue={setStartTimePeriod}
-                />
-              </View>
+              <Text style={tw`text-s text-gray-500 mb-2`}>Start Time</Text>
+<View style={tw`flex-row items-center mb-4`}>
+  <TextInput
+    style={tw`border p-2 rounded flex-1 mr-2 text-base h-10`} 
+    placeholder="7:00"
+    value={startTimeAdd}
+    onChangeText={setStartTimeAdd}
+  />
+  <DropDownPicker
+    placeholder='AM/PM'
+    open={startOpen}
+    setOpen={handleStartOpen}
+    containerStyle={tw`h-10 flex-1`}  
+    style={tw`border p-2 rounded bg-white text-black text-base h-10`}  
+    items={items}
+    setItems={setItems}
+    value={startValue}
+    setValue={setStartValue}
+    onChangeValue={setStartTimePeriod}
+  />
+</View>
 
-              <Text style={tw`text-xs text-gray-500 mb-2`}>End Time</Text>
-              <View style={tw`flex-row items-center mb-4`}>
-                <TextInput
-                  style={tw`border p-2 rounded flex-1 mr-2`}  // Added flex-1 and margin-right
-                  placeholder="9:00"
-                  value={endTimeAdd}
-                  onChangeText={setEndTimeAdd}
-                />
-                {!startOpen && (<DropDownPicker
-                  placeholder='AM/PM'
-                  open={endOpen}
-                  setOpen={handleEndOpen}
-                  containerStyle={tw`h-15 flex-1`}
-                  style={tw`border p-2 rounded bg-white text-black`}
-                  items={items}
-                  setItems={setItems}
-                  value={endValue}
-                  setValue={setEndValue}
-                  onChangeValue={setEndTimePeriod}
-                />)}
-              </View>
+<Text style={tw`text-s text-gray-500 mb-2`}>End Time</Text>
+<View style={tw`flex-row items-center mb-4`}>
+  <TextInput
+    style={tw`border p-2 rounded flex-1 mr-2 text-base h-10`} 
+    placeholder="9:00"
+    value={endTimeAdd}
+    onChangeText={setEndTimeAdd}
+  />
+  {!startOpen && (
+    <DropDownPicker
+      placeholder='AM/PM'
+      open={endOpen}
+      setOpen={handleEndOpen}
+      containerStyle={tw`h-10 flex-1`}  
+      style={tw`border  rounded bg-white text-black text-base`}  
+      items={items}
+      setItems={setItems}
+      value={endValue}
+      setValue={setEndValue}
+      onChangeValue={setEndTimePeriod}
+    />
+  )}
+</View>
 
               <View style={tw`flex-row justify-between`}>
                 <TouchableOpacity onPress={() => setModalVisible(false)}>
