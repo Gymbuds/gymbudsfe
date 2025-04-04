4bfc201a6f52cfb5d62c6e3f8e89ac2e840ce0dc
Vincent Li <vincentli402@gmail.com>
2025-04-04 14:24:23 -0400

    added location perms

--- react-native-healthd.ts
+++
@@ -1,6 +0,0 @@
-// types/react-native-health.d.ts
-declare module 'react-native-health' {
-    export interface AppleHealthKit {
-      requestAuthorization: (options: any, callback: (err: any, result: any) => void) => void;
-    }
-  }  
